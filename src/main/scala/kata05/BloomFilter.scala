3d301bc55585202277f6559019174c7cff5a73bb
Ivan Žiković <izikovic@gmail.com>
2015-08-04 15:29:24 +0200

    BloomFilter

--- src/main/scala/kata05/BloomFilter.scala
+++ src/main/scala/kata05/BloomFilter.scala
@@ -7,15 +7,51 @@ import scala.collection.immutable.BitSet
  */
 
 class Spellchecker(dictionary: List[Word]) {
-  
-  class BloomFilter(hashNumber: Int, bitmapSize: Int) {
 
-    val bitSet: BitSet = ???
-    
-    def hash(word: Word) = ???
-    
-    
+  class BloomFilter(hashNumber: Int, bitmapSize: Int) {
+    require(hashNumber > 0)
+
+    lazy val bitSet: BitSet = createBitSet(dictionary)
+
+    def hash(word: Word): List[Int] = {
+      import java.security.MessageDigest
+      val bytes = MessageDigest.getInstance("MD5").digest(word.getBytes)
+
+      val hashes =
+        for {
+          i <- 0 to bytes.length by (bytes.length / hashNumber + 1)
+        } yield normalize(bytes(i))
+
+      val hashList = hashes.toList
+      assert(hashList.size == hashNumber)
+
+      hashList
+    }
+
+    def normalize(n: Byte): Int = {
+      (n - Byte.MaxValue) * (bitmapSize) / (Byte.MaxValue - Byte.MinValue)
+    }
+
+    def createWordBitSet(w: Word): BitSet = {
+      val hashes = hash(w)
+      BitSet((hashes map Math.abs): _*)
+    }
+
+    def createBitSet(words: List[Word]): BitSet = {
+      def recursiveBitSet(words: List[Word], agg: BitSet): BitSet = words match {
+        case Nil => agg
+        case w :: ws => recursiveBitSet(ws, createWordBitSet(w) ++ agg)
+      }
+      recursiveBitSet(words, BitSet())
+    }
+
+    def contains(w: Word): Boolean = {
+      (bitSet & createWordBitSet(w)) == createWordBitSet(w)
+    }
   }
-  
-  
+
+  val filter = new BloomFilter(NumberOfHashes, BitSetSize)
+
+  def contains(w: Word) = filter.contains(w)
+
 }
